e4b4d29354ea07a85d2a8967c2c372fb8afaafc1
wuyiying2018 <2503206327@qq.com>
2023-12-06 00:24:14 -0500

    CardioStatsUSA

--- code.R
+++ code.R
@@ -2,3 +2,7 @@
 #           P8123  Final Project             #
 #   Yi Huang (yh3554) & Yiying Wu (yw3996)   #
 ##############################################
+
+# R packages
+# install.packages("CardioStatsUSA")
+library(CardioStatsUSA)
